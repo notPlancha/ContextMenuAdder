0b89500284865dfb1a2824c938d23ec71a386dbb
notPlancha <andre.plancha@hotmail.com>
2020-11-29 14:09:14 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import winreg as wreg
+
+def create_reg(name, path, message):
+    key = wreg.CreateKey(wreg.HKEY_CLASSES_ROOT, "*\\shell\\" + name)
+    wreg.SetValue(key, 'command', wreg.REG_SZ, ("\"{}\", \"%1\"".format(path)))
+    wreg.SetValueEx(key, '(Default)', 0, wreg.REG_SZ, message)
+    wreg.SetValueEx(key, 'Icon', 0, wreg.REG_SZ, "\"{}\"".format(path))
+    key.Close()
+    
+if __name__ == '__main__':
+    name = input("Name for reg folder: ")
+    path = input("path: ")
+    message = input("What message should appear? ")
+    
+    create_reg(name, path, message)
+    
+    print("Created; You should see the changes when you restart your pc.")
+    input("\nEnter to exit")
+
+
+
+#keyCreated = wreg.CreateKey(wreg.HKEY_CLASSES_ROOT, "\\shell")
+
